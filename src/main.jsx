d151197c343ac3d2bebfae37f1e98645ed4ef5ec
kevindermot99 <mbonimpayekevin@gmail.com>
2024-10-19 18:33:53 +0200

    PWA

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({ registerType: 'autoUpdate'})
+  ],
   css: {
     postcss: {
       plugins: [tailwindcss()],
