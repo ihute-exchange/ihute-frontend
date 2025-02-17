9b6335477c204841294321986612a8670a17145c
mbonikev <mbonimpayekevin@gmail.com>
2025-02-17 08:27:51 +0200

    service worker moved back

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import * as serviceWorkerRegistration from '/public/serviceWorkerRegistration';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration.js';
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
 
 
